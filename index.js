083f1c607228031c40def921e5b7fbcf693b45bb
kano <kanokanombola@gmail.com>
2022-02-04 21:25:29 +0200

    Add eventListener to submit button

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
-const menuBtnIcon = document.querySelector('i');
+const menuBtnIcon = document.querySelector("i");
 const menuBtnClass = menuBtnIcon.classList[1];
-const mobileNav = document.querySelector('.navMenu');
+const mobileNav = document.querySelector(".navMenu");
 
 function switchIcons() {
-    if (menuBtnClass == 'fa-bars') {
-    menuBtnIcon.classList.toggle('fa-times');
-  } 
+  if (menuBtnClass == "fa-bars") {
+    menuBtnIcon.classList.toggle("fa-times");
   }
+}
 
-mobileNav.addEventListener('click', ()=>{
+mobileNav.addEventListener("click", () => {
   switchIcons();
-  if(mobileNav.classList.contains('active')){
-    mobileNav.classList.remove('active');
-  }else{
-    mobileNav.classList.add('active');
-  } 
-})
+  if (mobileNav.classList.contains("active")) {
+    mobileNav.classList.remove("active");
+  } else {
+    mobileNav.classList.add("active");
+  }
+});
 
-// form validation 
-const submitBtn = document.getElementById("submitBtn"); 
+// form validation
+// const submitBtn = document.getElementById("submitBtn");
+const form = document.querySelector("form");
+form.addEventListener("submit", validator);
 
-function validator(e) { 
-  const formValue = document.getElementById("email").value; 
-  if (formValue === formValue.toLowerCase()) { 
-    console.log(formValue,e); 
-  } else { 
-    e.preventDefault(); 
-  } } 
-  
-submitBtn.addEventListener("click", validator); 
+function validator(e) {
+  const errorMessg = document.getAnimations("error");
+  const formValue = document.getElementById("email").value;
+  if (formValue === formValue.toLowerCase()) {
+  } else {
+    e.preventDefault();
+    console.log("not the correct format", errorMessg);
+    errorMessg.innerHTML +=
+      '<i class="fas fa-exclamation-triangle"></i> *please insert email in the correct format and do not include uppercase letters *';
+  }
+}
+
+submitBtn.addEventListener("click", validator);
